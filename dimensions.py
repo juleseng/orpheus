15cb020f5858d07e915c51c127b38764d1ef643b
Julianna Eng <juliannaeng@juliannas-mbp.lan>
2020-09-08 00:16:12 -0700

    update

--- dimensions.py
+++ dimensions.py
@@ -4,7 +4,7 @@ multiplied_list = [element * 0.375 for element in a_list]
 
 print(multiplied_list)
 
-a_list2 = [1844,515,2065,744, 2232,497,2517,720]
+a_list2 = [1477,926,1773,1267]
 
 multiplied_list2 = [element2 * .45 for element2 in a_list2]
 
